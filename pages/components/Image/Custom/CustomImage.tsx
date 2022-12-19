dc6833c8d1095d9489d35f279f5583c5fbe1e9c2
s-jez <stanislawjez7@gmail.com>
2022-12-19 17:36:36 +0100

    Add width zoom.

--- pages/components/Image/Custom/CustomImage.tsx
+++ pages/components/Image/Custom/CustomImage.tsx
@@ -1,23 +1,31 @@
 import Image from "next/image";
 import styles from "./CustomImage.module.scss";
 import { FC } from "react";
+import { url } from "inspector";
 
 interface ICustomImage {
   src: string;
   alt: string;
+  width: number;
+  height: number;
 }
 
-const CustomImage: FC<ICustomImage> = ({ src, alt }: ICustomImage) => {
+const CustomImage: FC<ICustomImage> = ({
+  src,
+  alt,
+  width,
+  height,
+}: ICustomImage) => {
   return (
-    <div>
+    <div className={styles.square} style={{ backgroundImage: `url(${src})` }}>
       <div className={styles.circle}>
         <Image
           className={styles.img}
           src={src}
           alt={alt}
-          width={256}
-          height={256}
-        />
+          width={width}
+          height={height}
+        ></Image>
       </div>
     </div>
   );
